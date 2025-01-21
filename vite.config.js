04682e663d8a4206df0974ec10d228621e7bc7e6
CoderYUI <bhaskar.23bey10012@vitbhopal.ac.in>
2025-01-21 15:52:10 +0530

    Bug Fix of Vercel & Vite Configuration and Redirection Issue

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
+
+export default defineConfig({
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        index: resolve(__dirname, 'index.html'),
+        tools: resolve(__dirname, 'tools-and-generator.html'),
+        card: resolve(__dirname, 'card-generator.html'),
+        grid: resolve(__dirname, 'grid-layout-generator.html'),
+        table: resolve(__dirname, 'table-generator.html'),
+        form: resolve(__dirname, 'form-generator.html')
+      }
+    }
+  },
+  publicDir: 'public'
+})
