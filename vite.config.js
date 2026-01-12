3fa93b47261e09deee0d810a89dcb37041636a31
Harsh-Panchal-1 <harsh.panchal1615@gmail.com>
2026-01-12 23:59:59 +0530

    Added Favicons and Fixed redirect bug to Tool & Generator Section from Server side

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
       input: {
         index: resolve(__dirname, 'index.html'),
         tools: resolve(__dirname, 'tools-and-generator.html'),
-        tools: resolve(__dirname, 'button-generator.html'),
+        button: resolve(__dirname, 'button-generator.html'),
         card: resolve(__dirname, 'card-generator.html'),
         grid: resolve(__dirname, 'grid-layout-generator.html'),
         table: resolve(__dirname, 'table-generator.html'),
