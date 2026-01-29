69c17703e176e18278e79046b301e1cb3105a690
Harsh-Panchal-1 <harsh.panchal1615@gmail.com>
2026-01-29 23:24:26 +0530

    Added the tool to generate Toast Notification

--- vite.config.js
+++ vite.config.js
@@ -15,6 +15,7 @@ export default defineConfig({
         tools: resolve(__dirname, 'src/pages/tools-and-generator.html'),
         button: resolve(__dirname, 'src/pages/button-generator.html'),
         card: resolve(__dirname, 'src/pages/card-generator.html'),
+        toast: resolve(__dirname, 'src/pages/toast-generator.html'),
         grid: resolve(__dirname, 'src/pages/grid-layout-generator.html'),
         table: resolve(__dirname, 'src/pages/table-generator.html'),
         form: resolve(__dirname, 'src/pages/form-generator.html'),
